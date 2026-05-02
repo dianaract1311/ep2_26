44930a636dc10dfcb4c32c14b92fb459a1b10977
Dianara <dianaracandiditavares@gmail.com>
2026-05-01 22:46:36 -0300

    terceira_versao

--- funcoes.py
+++ funcoes.py
@@ -25,3 +25,18 @@ def guardar_dado(dados_rolados, dados_no_estoque, dado_para_guardar):
     dados_no_estoque.append(dado)
     
     return [nova_lista, dados_no_estoque]
+
+def remover_dado(dados_rolados, dados_no_estoque, dado_para_remover):
+    dado = dados_no_estoque[dado_para_remover]
+    
+    nova_lista = []
+    i = 0
+    
+    while i < len(dados_no_estoque):
+        if i != dado_para_remover:
+            nova_lista.append(dados_no_estoque[i])
+        i += 1
+    
+    dados_rolados.append(dado)
+    
+    return [dados_rolados, nova_lista]
