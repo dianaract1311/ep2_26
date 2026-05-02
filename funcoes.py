cbd969a632b1cd0d99cbc7a6e7a83c61ac0c7462
Dianara <dianaracandiditavares@gmail.com>
2026-05-01 22:44:07 -0300

    segunda_versao

--- funcoes.py
+++ funcoes.py
@@ -10,3 +10,18 @@ def rolar_dados(n):
         i += 1
     
     return lista
+
+def guardar_dado(dados_rolados, dados_no_estoque, dado_para_guardar):
+    dado = dados_rolados[dado_para_guardar]
+    
+    nova_lista = []
+    i = 0
+    
+    while i < len(dados_rolados):
+        if i != dado_para_guardar:
+            nova_lista.append(dados_rolados[i])
+        i += 1
+    
+    dados_no_estoque.append(dado)
+    
+    return [nova_lista, dados_no_estoque]
